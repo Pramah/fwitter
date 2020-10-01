eee28c984bbabcfb9f418dafb8f8093697111498
prasanna <bagilip@gmail.com>
2020-09-30 19:54:32 -0700

    DATABASE TABLE

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,10 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
+
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'me',
-  password: 'secret',
-  database: 'my_db',
-});
+  user: 'root',
+  password: 'password',
+  database: 'fwitter_db',
+}).promise();
+
+module.exports = connection;
---
+++ model/fwitter_schema.sql
@@ -0,0 +1,10 @@
+
+CREATE DATABASE fwitter_db;
+
+USE fwitter_db;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT NOT NULL,
+    username VARCHAR(16) NOT NULL,
+    PRIMARY KEY(id)
+);
